0d916bc30bd4a8da93c38e6d623b0c5eeaa1de7d
silvanocastro <silvanocastro.sc@gmail.com>
2024-09-07 17:00:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Stephan August",
+        descricao: "O gay mais gay de todos, o rei dos gay vindo do Parana um verdadeiro gay do pé vermelho",
+        link: "https://www.instagram.com/stephanaugust?igsh=MWo3aW54ZDdnN2ltYQ==",
+        tags: "parana feio viado gay paranaids calça bolso barbudo",
+    },
+    {
+        titulo: "Guilherme M Moser",
+        descricao: "Um gay sofisticado, apenas é gay em caso necessário",
+        link: "https://www.instagram.com/guilhermemanerichi/?igsh=MWZzNG90aGw4d2NxZA%3D%3D",
+        tags: "gui guiga moser manerich joinville borat fred mercury",
+    },
+    {
+        titulo: "Juliano Masson",
+        descricao: "Salin não é gay porque ser gay gasta o cu",
+        link: "https://www.instagram.com/julianomasson?igsh=NThqa3g1eW40MHlm",
+        tags: "salin masson gaxpar gaspar juju jp lindao lindão jplindão jplindao",
+    }
+];
